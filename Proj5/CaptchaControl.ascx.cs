a83daf96c50e4d893701276273d380ee010f0788
Kenny Le <42878549+LeKenny7@users.noreply.github.com>
2020-11-19 23:38:44 -0700

    Revert "Revert "Add Captcha""
    This reverts commit 2cc8e6ccd9053f78f8945f21a5a69e7072d14b3b.

---
+++ Proj5/CaptchaControl.ascx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proj5
+{
+    public partial class CaptchaControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void verifyButton_Click(object sender, EventArgs e) //Checks if correctly entered captcha
+        {
+            string userInput = CaptchaCode.Text;
+            bool correctCaptcha = ExampleCaptcha.Validate(userInput); //returns if captcha is correct
+            CaptchaCode.Text = null;    //clears the previous text
+
+            if (correctCaptcha)
+            {
+                captchaLabel.Text = "Correct input!";
+                Response.Redirect("Register.aspx"); //Go to zipcode page
+                // TODO: proceed with protected action
+            }
+            else
+            {
+                captchaLabel.Text = "Wrong input!"; //Redoo
+            }
+        }
+    }
+}
