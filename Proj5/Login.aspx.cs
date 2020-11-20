d781ce01ff66ffe2e1d60350d2b709ddb8adcd61
Kenny Le <42878549+LeKenny7@users.noreply.github.com>
2020-11-20 03:04:34 -0700

    Added Captcha and Cookies

--- Proj5/Login.aspx.cs
+++ Proj5/Login.aspx.cs
@@ -97,7 +97,7 @@ namespace Proj5
 
         protected void LinkButton1_Click(object sender, EventArgs e)
         {
-            Response.Redirect("Register.aspx");
+            Response.Redirect("CaptchaForm.aspx");
         }
     }
 }
